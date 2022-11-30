5741770e5d9abb0af669f88fcded273145683aa3
LucasPCotrim <lucaspcotrim@gmail.com>
2022-11-30 20:39:57 -0300

    fix: Added type getBookingResult to function getBooking on booking-service

--- src/services/booking-service/index.ts
+++ src/services/booking-service/index.ts
@@ -1,13 +1,15 @@
 import bookingRepository from "@/repositories/booking-repository";
 import { notFoundError } from "@/errors";
+import { Booking, Room } from "@prisma/client";
 
-async function getBooking(userId: number) {
+async function getBooking(userId: number): Promise<getBookingResult[]> {
   const booking = await bookingRepository.findBookingByUserId(userId);
   if (!booking) {
     throw notFoundError();
   }
   return booking;
 }
+type getBookingResult = Omit<Booking, "userId" | "createdAt" | "updatedAt"> & { Room: Room[] };
 
 const bookingService = { getBooking };
 
